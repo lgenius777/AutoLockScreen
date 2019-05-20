dfaf6377cd9524edac03c4d7fdae8453b0557eb0
lgenius777 <lgenius777@163.com>
2019-05-20 23:21:57 +0800

    update main py

--- autoLock.py
+++ autoLock.py
@@ -1,3 +1,31 @@
 import findFace as finder;
 import lockScreen as locker;
 import cv2;
+import collections
+
+
+sampling_interval_ms =100;
+cycle_ms = 10*1000;
+sampling_site = int(cycle_ms/sampling_interval_ms);
+recorder = collections.deque((1,),maxlen=sampling_site);
+
+cap = cv2.VideoCapture(0)
+index = 0;
+lock_status = False;
+
+while cap.isOpened():
+    ret,frame = cap.read();
+    frame_new, hasFace = finder.findFace(frame);
+    c = cv2.waitKey(sampling_interval_ms);
+    if hasFace == False : recorder.append(1);
+    else : recorder.append(0);
+    if lock_status == False :
+        if recorder.count(1) > (sampling_site*0.8) :
+            locker.lockScreen();
+            lock_status = True;
+        # cv2.imshow('Face Detector', frame_new);       
+    else :
+        if recorder.count(1) < (sampling_site*0.8) :
+            lock_status = False;
+
+cap.release()
--- findFace.py
+++ findFace.py
@@ -1,12 +1,14 @@
-import cv2
-# 获取训练好的人脸的参数数据，这里直接从GitHub上使用默认值
-face_cascade = cv2.CascadeClassifier(r"C:\Program Files\Python37\Lib\site-packages\cv2\data\haarcascade_eye.xml")
-test = face_cascade.load(r"C:\Program Files\Python37\Lib\site-packages\cv2\data\haarcascade_eye.xml")
+import os;
+import cv2;
+cv_lib_path = os.path.dirname(cv2.__file__);
+haarcascade_path = os.path.join(cv_lib_path, "data", "haarcascade_eye.xml");
+face_cascade = cv2.CascadeClassifier(haarcascade_path);
+test = face_cascade.load(haarcascade_path);
 
 def findFace(frame):
     frame_gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces=face_cascade.detectMultiScale(frame_gray,1.3,5)
-    print ("发现{0}个人脸!".format(len(faces)));
+    print ("Find {0} target!".format(len(faces)));
     hasFace = (len(faces) != 0);
     if hasFace:
         for(x,y,w,h) in faces:
